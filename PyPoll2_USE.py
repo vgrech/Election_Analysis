4bff3252b110ddf01a0d528cb97095c63bcdcc8f
vgrech <85713466+vgrech@users.noreply.github.com>
2021-06-30 21:36:42 -0400

    Fixed Code Errors

--- PyPoll2_USE.py
+++ PyPoll2_USE.py
@@ -4,7 +4,7 @@ import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Election_Analysis", "Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("Election_Analysis","analysis", "election_analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -13,6 +13,10 @@ total_votes = 0
 candidate_options = []
 # 1. Declare the empty dictionary.
 candidate_votes = {}
+#Track the winning candidate, vote count and percentage
+winngin_candidate=""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -25,21 +29,23 @@ with open(file_to_load) as election_data:
     for row in file_reader:
         # Add to the total vote count.
         total_votes += 1
-
-        # Print the candidate name from each row.
+        # Get the candidate name from each row.
         candidate_name = row[2]
-
-    if candidate_name not in candidate_options:
-
-        # Add the candidate name to the candidate list.
-        candidate_options.append(candidate_name)
-
-        # Begin tracking that candidate's vote count.
-        candidate_votes[candidate_name] = 0
-
+#If the candidate does not match any existing candidate add it in 
+#the candidate list.
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+            # Begin tracking that candidate's voter count.
+            candidate_votes[candidate_name] = 0
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
+for candidate_name in candidate_votes:
+    #Retrieve votes count and percentage.
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes) / float(total_votes) * 100
+    #Print ea
 # Winning Candidate and Winning Count Tracker
 winning_candidate = ""
 winning_count = 0
@@ -52,27 +58,20 @@ for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name]
     # Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
+    #Print each candidate, their voter count, and percentage to the terminal
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    #To do: print out each candidate's name, vote count, and percentage of
-    # votes to the terminal.
-
-    # Determine winning vote count and candidate
-    # Determine if the votes is greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-         # If true then set winning_count = votes and winning_percent =
-         # vote_percentage.
+    #Determine winnning vote count, winning percentage, and candidate.
+    if(votes > winning_count) and (vote_percentage > winning_percentage):
         winning_count = votes
-        winning_percentage = vote_percentage
-         # And, set the winning_candidate equal to the candidate's name.
         winning_candidate = candidate_name
-
-#  To do: print out each candidate's name, vote count, and percentage of
-# votes to the terminal.
-print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        winning_percentage = vote_percentage
+#Print the winning candidates' results to the terminal.
 winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
+    f"------------------------\n"
+    f"Winner:{winning_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
+
 print(winning_candidate_summary)
