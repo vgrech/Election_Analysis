12a7fc44d89f3386e01043b088b914697e28c858
vgrech <85713466+vgrech@users.noreply.github.com>
2021-06-30 22:06:59 -0400

    Printed Election Results

--- PyPoll2_USE.py
+++ PyPoll2_USE.py
@@ -41,37 +41,50 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-for candidate_name in candidate_votes:
-    #Retrieve votes count and percentage.
-    votes = candidate_votes[candidate_name]
-    vote_percentage = float(votes) / float(total_votes) * 100
-    #Print ea
-# Winning Candidate and Winning Count Tracker
-winning_candidate = ""
-winning_count = 0
-winning_percentage = 0
+        #Save the results to our text file.
+        with open(file_to_save, "w") as txt_file:
 
-# Determine the percentage of votes for each candidate by looping through the counts.
-# Iterate through the candidate list.
-for candidate_name in candidate_votes:
-    # Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
-    # Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-    #Print each candidate, their voter count, and percentage to the terminal
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            #Print the final vote count to the terminal.
+            election_results = (
+                f"\nElections Results\n"
+                f"-------------------------\n"
+                f"Total Votes: {total_votes:,}\n"
+                f"-------------------------\n")
+            print(election_results, end="")
+            #Save the final vote count to the text file.
+            txt_file.write(election_results)
 
-    #Determine winnning vote count, winning percentage, and candidate.
-    if(votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_candidate = candidate_name
-        winning_percentage = vote_percentage
-#Print the winning candidates' results to the terminal.
-winning_candidate_summary = (
-    f"------------------------\n"
-    f"Winner:{winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
+    for candidate_name in candidate_votes:
+        #Retrieve votes count and percentage.
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        
+    # Winning Candidate and Winning Count Tracker
+    winning_candidate = ""
+    winning_count = 0
+    winning_percentage = 0
 
-print(winning_candidate_summary)
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        #Print each candidate, their voter count, and percentage to the terminal
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        #Determine winnning vote count, winning percentage, and candidate.
+        if(votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_candidate = candidate_name
+            winning_percentage = vote_percentage
+    #Print the winning candidates' results to the terminal.
+    winning_candidate_summary = (
+        f"------------------------\n"
+        f"Winner:{winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+
+    #print(winning_candidate_summary)
